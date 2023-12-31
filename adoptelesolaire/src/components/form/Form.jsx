9a52fca89c7e815b9cb6a978eb790face3f55661
Master Pampa <styven.munhurrun@gmail.com>
2023-12-31 12:56:01 +0100

    consentement donnée

--- adoptelesolaire/src/components/form/Form.jsx
+++ adoptelesolaire/src/components/form/Form.jsx
@@ -38,6 +38,7 @@ function Form() {
   
   const [emailError, setEmailError] = useState('');
   const [phoneError, setPhoneError] = useState('');
+  const [consentChecked, setConsentChecked] = useState(true);
 
   const backPage = () => {
     if (page > 1) {
@@ -65,96 +66,104 @@ function Form() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+  
     if (validateForm()) {
+      if (consentChecked || window.confirm("Êtes-vous sûr de ne pas vouloir conserver les résultats de votre simulation ?")) {
+        // Calculs basés sur les données du formulaire
+        const pc = 3; // Puissance crête (3 kWc pour un kit classique)
+        const irr = 1450; // Irradiation de 1450 kWh/m2.an dans la région
+        const fc = parseFloat(formData.exposition); // Conversion de la valeur en nombre
+      
+        const productionAnnuelle = pc * irr * fc;
+      
+        const consotype = formData.consotype;
+        let consoValue;
     
-    // Calculs basés sur les données du formulaire
-    const pc = 3; // Puissance crête (3 kWc pour un kit classique)
-    const irr = 1450; // Irradiation de 1450 kWh/m2.an dans la région
-    const fc = parseFloat(formData.exposition); // Conversion de la valeur en nombre
+        if (consotype === "2") {
+          // Le montant de la facture mensuel en euro
+          consoValue = parseFloat(formData.consovalue)/0.2276*12;
+        } else if (consotype === "1") {
+          // La consommation en kWh mensuel
+          consoValue = parseFloat(formData.consovalue) * 12;
+        } else if (consotype === "3") {
+          // La consommation en kWh annuelle
+          consoValue = parseFloat(formData.consovalue);
+        } else if (consotype === "4") {
+          // La consommation en euro annuelle
+          consoValue = parseFloat(formData.consovalue)/0.2276;
+        } else {
+          window.alert('Type de consommation non pris en charge.');
+          return; // Quitte la fonction en cas de type de consommation non pris en charge
+        }
     
-    const productionAnnuelle = pc * irr * fc;
-    
-    const consotype = formData.consotype;
-    let consoValue;
+        const tac = (productionAnnuelle / consoValue) * 100;
+        const tacAvecPilotage = tac + 20;
+        
+        const potentielAutoconsommation = (productionAnnuelle * tacAvecPilotage) / 100;
+        
+        const gainReventeSurplus = productionAnnuelle - potentielAutoconsommation;
+        const gainEurosReventeSurplus = gainReventeSurplus * 0.13;
+        
+        const pourcentageEconomies = (potentielAutoconsommation / consoValue) * 100;
+        const pourcentageEconomiesAvecContratEDF = pourcentageEconomies + 8;
+      
+        const gainEnEurosSurFacture = pourcentageEconomiesAvecContratEDF * consoValue * 0.2276 / 100;
+        const amortissementAnnuel = ((gainEnEurosSurFacture + gainEurosReventeSurplus) / (6490 - 1530)) * 100;
   
-    if (consotype === "2") {
-      // Le montant de la facture mensuel en euro
-      consoValue = parseFloat(formData.consovalue)/0.2276*12;
-    } else if (consotype === "1") {
-      // La consommation en kWh mensuel
-      consoValue = parseFloat(formData.consovalue) * 12;
-    } else if (consotype === "3") {
-      // La consommation en kWh annuelle
-      consoValue = parseFloat(formData.consovalue);
-    } else if (consotype === "4") {
-      // La consommation en euro annuelle
-      consoValue = parseFloat(formData.consovalue)/0.2276;
+        const surface = formData.surface;
+    
+        // Affichage des résultats dans la console
+        console.log('Production annuelle en kWh:', productionAnnuelle);
+        console.log('Taux d\'autoconsommation:', tacAvecPilotage);
+        console.log('Potentiel de production autoconsommée en kWh:', potentielAutoconsommation);
+        console.log('Gain en euros avec la revente de surplus à l\'année:', gainEurosReventeSurplus);
+        console.log('Pourcentage d\'économies sur la facture annuelle:', pourcentageEconomiesAvecContratEDF);
+        console.log('Gain en euros d\'économies sur la facture à l\'année:', gainEnEurosSurFacture);
+        console.log('Amortissement de l\'installation sur l\'année:', amortissementAnnuel);
+    
+        try {
+          // Vérifier si le consentement est donné avant d'envoyer les données
+          if (consentChecked) {
+            const response = await fetch('http://localhost:3001/submit-form', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(formData),
+            });
+  
+            if (response.ok) {
+              console.log('Données envoyées avec succès.');
+            } else {
+              console.error('Erreur lors de l\'envoi des données.');
+            }
+          }
+  
+          navigate('/results', {
+            state: {
+              productionAnnuelle,
+              consoValue,
+              tacAvecPilotage,
+              potentielAutoconsommation,
+              gainReventeSurplus,
+              gainEurosReventeSurplus,
+              pourcentageEconomiesAvecContratEDF,
+              gainEnEurosSurFacture,
+              amortissementAnnuel,
+              surface,
+            },
+          });
+        } catch (error) {
+          console.error('Erreur inattendue :', error);
+        }
+      } else {
+        window.alert('Veuillez remplir tous les champs obligatoires.');
+      }
     } else {
-      window.alert('Type de consommation non pris en charge.');
-      return; // Quitte la fonction en cas de type de consommation non pris en charge
+      window.alert('Veuillez remplir tous les champs obligatoires.');
     }
+  };
   
-    const tac = (productionAnnuelle / consoValue) * 100;
-    const tacAvecPilotage = tac + 20;
-    
-    const potentielAutoconsommation = (productionAnnuelle * tacAvecPilotage) / 100;
-    
-    const gainReventeSurplus = productionAnnuelle - potentielAutoconsommation;
-    const gainEurosReventeSurplus = gainReventeSurplus * 0.13;
-    
-    const pourcentageEconomies = (potentielAutoconsommation / consoValue) * 100;
-    const pourcentageEconomiesAvecContratEDF = pourcentageEconomies + 8;
-  
-    const gainEnEurosSurFacture = pourcentageEconomiesAvecContratEDF * consoValue * 0.2276 / 100;
-    const amortissementAnnuel = ((gainEnEurosSurFacture + gainEurosReventeSurplus) / (6490 - 1530)) * 100;
-
-    const surface = formData.surface;
-  
-  // Affichage des résultats dans la console
-  console.log('Production annuelle en kWh:', productionAnnuelle);
-  console.log('Taux d\'autoconsommation:', tacAvecPilotage);
-  console.log('Potentiel de production autoconsommée en kWh:', potentielAutoconsommation);
-  console.log('Gain en euros avec la revente de surplus à l\'année:', gainEurosReventeSurplus);
-  console.log('Pourcentage d\'économies sur la facture annuelle:', pourcentageEconomiesAvecContratEDF);
-  console.log('Gain en euros d\'économies sur la facture à l\'année:', gainEnEurosSurFacture);
-  console.log('Amortissement de l\'installation sur l\'année:', amortissementAnnuel);
-  
-  try {
-    const response = await fetch('http://localhost:3001/submit-form', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-
-    if (response.ok) {
-      console.log('Données envoyées avec succès.');
-      navigate('/results', {
-        state: {
-          productionAnnuelle,
-          consoValue,
-          tacAvecPilotage,
-          potentielAutoconsommation,
-          gainReventeSurplus,
-          gainEurosReventeSurplus,
-          pourcentageEconomiesAvecContratEDF,
-          gainEnEurosSurFacture,
-          amortissementAnnuel,
-          surface,
-        },
-      });
-    } else {
-      console.error('Erreur lors de l\'envoi des données.');
-    }
-  } catch (error) {
-    console.error('Erreur inattendue :', error);
-  }
-}else {
-  window.alert('Veuillez remplir tous les champs obligatoires.');
-};
-}
 
 
 const validateForm = () => {
@@ -169,17 +178,23 @@ const validateForm = () => {
         formData.consovalue.trim() !== '' &&
         formData.vehicules.trim() !== ''
       );
-      case 4:
-        const isEmailValidFlag = isEmailValid(formData.mail);
-        const isPhoneNumberValidFlag = isPhoneNumberValid(formData.tel);
-  
-        setEmailError(isEmailValidFlag ? '' : 'Veuillez entrer une adresse e-mail valide.');
-        setPhoneError(isPhoneNumberValidFlag ? '' : 'Veuillez entrer un numéro de téléphone valide.');
-  
-        return isEmailValidFlag && isPhoneNumberValidFlag;
-      default:
-        return true;
-    }
+    case 4:
+      // Vérifier le consentement
+      if (!consentChecked) {
+        return true; // Le consentement n'est pas nécessaire, donc retourne true
+      }
+
+      // Vérifier l'email et le numéro de téléphone uniquement si le consentement est donné
+      const isEmailValidFlag = isEmailValid(formData.mail);
+      const isPhoneNumberValidFlag = isPhoneNumberValid(formData.tel);
+
+      setEmailError(isEmailValidFlag ? '' : 'Veuillez entrer une adresse e-mail valide.');
+      setPhoneError(isPhoneNumberValidFlag ? '' : 'Veuillez entrer un numéro de téléphone valide.');
+
+      return isEmailValidFlag && isPhoneNumberValidFlag;
+    default:
+      return true;
+  }
 };
   return (
     <span className="span">
@@ -341,29 +356,52 @@ const validateForm = () => {
         <div>
             <h2>Vous</h2>
             <form onSubmit={handleSubmit} name='formulaire4'>
-                <label>
-                    Votre adresse mail :
-                    <input
-                    type="email"
-                    name="mail"
-                    value={formData.mail}
-                    placeholder='Adresse email valide'
-                    onChange={handleChange}
-                    />
-                </label>
-                <label>
-                    Votre numéro de télephone :
-                    <input
-                      type ="tel"
-                      name="tel" maxLength="10"
-                      placeholder='Votre numéro de contact'
-                      onChange={handleChange}
-                    /> 
-                </label>
-                <input type='submit' value="Envoyer" className='submit'/>
-            </form>
-        </div>
+            <label className='consent'>
+        Recevoir votre étude personalisée :
+        <input
+          type="checkbox"
+          name="consent"
+          value="true"
+          checked={consentChecked}
+          onChange={() => setConsentChecked(!consentChecked)}
+        />
+      </label>
+
+      {/* Afficher les champs email et numéro uniquement si le consentement est donné */}
+      {consentChecked && (
+        <>
+          <label>
+            Votre adresse mail :
+            <input className='check'
+              type="email"
+              name="mail"
+              value={formData.mail}
+              placeholder='Adresse email valide'
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Votre numéro de télephone :
+            <input
+              type="tel"
+              name="tel"
+              maxLength="10"
+              placeholder='Votre numéro de contact'
+              onChange={handleChange}
+            />
+          </label>
+        </>
       )}
+
+      <input type='submit' value="Envoyer" className='submit'/>
+      {consentChecked && ( 
+        <>
+        <p className='mention'>En soumettant ce formulaire, j'accepte que les données saisie soient utilisées à des fins de relation commerciale.</p>
+        </>
+      )}
+    </form>
+  </div>
+)}
       {page > 1 && (
         <span onClick={handleBack} className='backButton'><p>Retour</p></span> 
       )}
